d1f7e73a1fc737bb82649cec6069e0c45b141a9d
RAHUL BAJAJ <rbajaj1998official@gmail.com>
2020-08-08 06:01:54 +0530

    chat room backend added

--- src/index.js
+++ src/index.js
@@ -2,17 +2,60 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const cors = require('cors')
+
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
+const router = require('./router');
 
-const publicDirectoryPath = path.join(__dirname, './public')
-app.use(express.static(publicDirectoryPath))
+app.use(cors());
+app.use(router);
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+// const publicDirectoryPath = path.join(__dirname, './public')
+// app.use(express.static(publicDirectoryPath))
 
 io.on('connection', () => {
     console.log("Connection Established")
-})
+
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if(error) return callback(error);
+
+        socket.join(user.room);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+        callback();
+    });
+
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        }
+    })
+});
+
+
+
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
